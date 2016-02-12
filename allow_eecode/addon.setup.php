3c5969646f3f0e3f3c48dabc2c8e453ac66b6c95
Derek Jones <derek.jones@ellislab.com>
2016-02-12 15:28:45 -0800

    finalizing builds for GitHub downloads

--- allow_eecode/addon.setup.php
+++ allow_eecode/addon.setup.php
@@ -5,7 +5,7 @@ return array(
 	'author_url'  => 'https://ellislab.com/',
 	'name'        => 'Allow EE Code',
 	'description' => 'Allows ExpressionEngine code to be used in entries',
-	'version'     => '2.0.1',
+	'version'     => '2.0.2',
 	'namespace'   => 'User\Addons\AllowEecode',
 	'settings_exist' => FALSE
 );
